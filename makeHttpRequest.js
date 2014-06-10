e06a7c7a6507bdd4e21d705826dabfc37013061b
Boris Borshevsky <borisborshevsky@gmail.com>
2014-06-10 18:53:07 +0300

    Add makeHttpRequest

---
+++ makeHttpRequest.js
@@ -0,0 +1,31 @@
+
+http = require("http")
+
+
+
+
+
+  var options = {
+    host: 'localhost',
+    port: 8080,
+    path: '/lucky/users',
+    method: 'POST'
+  };
+
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+
+  // write data to request body
+  req.write('{"username":"test2"}');
+  req.end();
+
