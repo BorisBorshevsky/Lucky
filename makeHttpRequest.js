4fc54002b8d88e46388619b0434a57f7db1a8d0a
Boris Borshevsky <borisborshevsky@gmail.com>
2014-06-11 14:44:22 +0300

    update make http request

--- makeHttpRequest.js
+++ makeHttpRequest.js
@@ -1,23 +1,24 @@
 
 http = require("http")
 
+var makeHttpPostRequest = function (host, port, path, data, callback) {
 
-
-
-
-  var options = {
-    host: 'localhost',
-    port: 8080,
-    path: '/lucky/users',
+    var options = {
+    host: host,
+    port: port,
+    path: path,
     method: 'POST'
   };
 
   var req = http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    //console.log('STATUS: ' + res.statusCode);
+    //console.log('HEADERS: ' + JSON.stringify(res.headers));
+    if (res.statusCode == 200)
+      callback(res.statusCode)
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
+      callback(res.statusCode,chunk)
+    //  console.log('BODY: ' + chunk);
     });
   });
 
@@ -25,7 +26,65 @@ http = require("http")
     console.log('problem with request: ' + e.message);
   });
 
-  // write data to request body
-  req.write('{"username":"test2"}');
-  req.end();
 
+  req.write( JSON.stringify (data) )
+  req.end();
+}
+
+
+/*
+var newUser ={
+  username: "boris5",
+  password: "123123",
+  kosher: true,
+  styles: ["aaa","bbb","ccc"]
+}
+
+makeHttpPostRequest('localhost',8080,'/lucky/users',newUser,function(code,message){
+  if(code == 200)
+    console.log("post success")
+  else
+    console.log("Error: " + message) 
+})
+
+*/
+
+
+
+
+
+var makeHttpGetRequest = function (host, port, path, callback) {
+  
+  var options = {
+    host: host,
+    port: port,
+    path: path
+  };
+
+  http.get(options, function(res) {
+    //console.log("Got response: " + res.statusCode);
+    res.on("data", function(chunk) {
+      //console.log("BODY: " + chunk);
+      callback(res.statusCode,chunk)  
+    });
+
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    callback(e.message)
+  });
+
+};
+
+module.exports.makeHttpGetRequest = makeHttpGetRequest
+module.exports.makeHttpPostRequest = makeHttpPostRequest
+
+/*
+makeHttpGetRequest('localhost',8080,'/lucky/users/test3',function(code,res){
+  if (code != 200){
+    console.log("ErrorCode: " + code + ", Massege: " + res)
+  }else{
+    console.log("ErrorCode: " + code + ", Massege: " + res)  
+  }
+});
+
+*/
